e27421ac5515294b1d21983f854ca7d9e12c686f
Zhivko Georgiev <zhivko.georgiev@bulpros.com>
2017-02-05 12:28:35 +0200

    Write to Redis list finalized.

--- src/main/java/com/sentiment/data/writer/RedisDataWriter.java
+++ src/main/java/com/sentiment/data/writer/RedisDataWriter.java
@@ -5,11 +5,14 @@ import java.util.List;
 import redis.clients.jedis.Jedis;
 
 public class RedisDataWriter implements DataWriter {
-	private final Jedis jedisClient = new Jedis("localhost"); 
+	private static final String REDIS_HOSTNAME = "localhost";
+	private static final String REDIS_LIST_KEY = "fb-posts_comments";
+	
+	private final Jedis jedisClient = new Jedis(REDIS_HOSTNAME); 
 
 	@Override
 	public void write(List<String> data) {
 		data.stream()
-			.forEach(jedisClient::lpush);
+			.forEach(string -> jedisClient.lpush(REDIS_LIST_KEY, string));
 	}
 }
